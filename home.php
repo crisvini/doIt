d91a93a41e3d4d34a8f8d0c0b29214220cba5302
Cristian <crisvini.leoncini@outlook.com>
2022-09-22 20:30:38 -0300

    início da home

--- home.php
+++ home.php
@@ -1,3 +1,17 @@
 <?php
+session_start();
+include_once("./includes/includes.php");
+?>
 
-echo 'home';
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php Componentes::head('Home'); ?>
+</head>
+
+<body>
+    <i class="fa-solid fa-list-check"></i>
+</body>
+
+</html>
