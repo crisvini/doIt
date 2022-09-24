42229bc2e9dfbed76278eb72aaff03af5698a322
Cristian <crisvini.leoncini@outlook.com>
2022-09-23 23:23:39 -0300

    adição de componente de header

--- home.php
+++ home.php
@@ -1,5 +1,9 @@
 <?php
+// Inclusão do arquivo de includes
 include_once("./includes/includes.php");
+// Inclusão do arquivo de conexão
+include("./connection/conexao.php");
+// Função que verifica se o usuário está autenticado
 Funcoes::verificaLogin();
 ?>
 
@@ -7,50 +11,37 @@ Funcoes::verificaLogin();
 <html lang="pt-br">
 
 <head>
+    <!-- Inclusão do head -->
     <?php Componentes::head('Home'); ?>
 </head>
 
 <body>
+    <!-- Navbar/Header -->
     <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><span class="fs-2 text-success fw-bold">DoIt <i class="fa-solid fa-list-check"></i></span></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="navbar-toggler-icon fa-solid fa-bars h-100 fs-2 text-success"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarScroll">
-                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                        <li class="nav-item">
-                            <a class="nav-link text-success fs-6 fw-bolder" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-success fs-6 fw-bolder" href="#">Link</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex" role="search">
-                        <span class="text-success fw-bold" style="font-size: 110%;">Sair&nbsp;<i class="fa-solid fa-right-from-bracket"></i></span>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        <!-- Inclusão do navbar/header -->
+        <?php Componentes::header(); ?>
     </header>
 
     <main class="mt-3 mt-lg-4 container">
         <div class="px-4 py-3 rounded bg-light">
+            <!-- Header tabela     -->
             <div class="row mt-2 mb-4">
                 <div class="col">
                     <span class="fw-bolder fs-3 text-success"><i class="fa-solid fa-list-check"></i>&nbsp;Tarefas</span>
                 </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-6 text-start">
-                    <button class="btn btn-success rounded" id="inserir_btn">Inserir tarefa</button>
-                </div>
-                <div class="col-6 text-end align-self-center">
-                    <i class="pointer fa-solid fa-file-excel text-success fs-4 me-3 me-lg-4"></i>
-                    <i class="pointer fa-solid fa-file-pdf text-success fs-4"></i>
+                <div class="col-6 text-end align-self-end">
+                    <i class="pointer success-hover fa-solid fa-file-excel text-success fs-4 me-3 me-lg-4"></i>
+                    <i class="pointer success-hover fa-solid fa-file-pdf text-success fs-4"></i>
                 </div>
             </div>
+
+            <?php
+
+                
+
+            ?>
+
+            <!-- Tabela -->
             <div class="table-responsive">
                 <table class="table table-hover">
                     <thead>
@@ -71,8 +62,8 @@ Funcoes::verificaLogin();
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td class="text-success text-center"><i class="pointer fa-solid fa-pen-to-square"></i></td>
-                            <td class="text-success text-center"><i class="pointer fa-solid fa-trash"></i></td>
+                            <td class="text-success text-center"><i class="pointer success-hover fa-solid fa-pen-to-square"></i></td>
+                            <td class="text-success text-center"><i class="pointer success-hover fa-solid fa-trash"></i></td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
@@ -80,8 +71,8 @@ Funcoes::verificaLogin();
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td class="text-success text-center pointer"><i class="fa-solid fa-pen-to-square"></i></td>
-                            <td class="text-success text-center pointer"><i class="fa-solid fa-trash"></i></td>
+                            <td class="text-success text-center pointer"><i class="pointer success-hover fa-solid fa-pen-to-square"></i></td>
+                            <td class="text-success text-center pointer"><i class="pointer success-hover fa-solid fa-trash"></i></td>
                         </tr>
                         <tr>
                             <th scope="row">3</th>
@@ -89,12 +80,16 @@ Funcoes::verificaLogin();
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td class="text-success text-center"><i class="pointer fa-solid fa-pen-to-square"></i></td>
-                            <td class="text-success text-center"><i class="pointer fa-solid fa-trash"></i></td>
+                            <td class="text-success text-center"><i class="pointer success-hover fa-solid fa-pen-to-square"></i></td>
+                            <td class="text-success text-center"><i class="pointer success-hover fa-solid fa-trash"></i></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+        </div>
+        <!-- Floating Button (incluir nova tarefa) -->
+        <div class="floating-btn pointer success-hover-background">
+            <i class="fa fa-plus plus"></i>
         </div>
     </main>
 </body>
