0fd152d98a2b421cafe5cda2fb9d71bcde3a2f56
Cristian <crisvini.leoncini@outlook.com>
2022-09-25 11:33:21 -0300

    correção na correção da data de conclusão

--- ajax/editarTarefa.php
+++ ajax/editarTarefa.php
@@ -8,7 +8,8 @@ $sql = "UPDATE
         SET
             nome = '" . $_POST["nome"] . "',
             descricao = '" . $_POST["descricao"] . "',
-            _id_status = " . $_POST["status"] . "
+            _id_status = " . $_POST["status"] . ",
+            data_conclusao = IF (" . $_POST["status"] . " = 2, CURRENT_TIMESTAMP(), NULL)
         WHERE
             id_tarefa = " . (int) $_POST["id_tarefa"];
 $result = $mysqli->query($sql);
